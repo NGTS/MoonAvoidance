115ba0bc327ac7f77b8347b93ddf14b91c0a7861
jmccormac01 <jmccormac001@gmail.com>
2016-01-23 07:13:06 +0000

    major update to moon avoidance script:   included overscan correction of data before stats   included ds9 checking of images below ghostlim   included montage of images below ghostlim

--- moonAvoidance.py
+++ moonAvoidance.py
@@ -2,10 +2,25 @@
 # level and plot the results versus moon angle
 from astropy.io import fits
 import matplotlib.pyplot as pl
+from matplotlib import cm
 from scipy.optimize import curve_fit
+import argparse as ap
 import numpy as np
 import glob as g
-import seaborn, os
+import seaborn, os, time
+from math import sqrt,ceil
+from ds9 import *
+
+# get command line args
+def argParse():
+	parser=ap.ArgumentParser(description="A script measure the moon avoidance angle")
+	parser.add_argument('--ghostlim',type=int,help = "Angle below which to check images for ghosts")
+	parser.add_argument('--ds9',help = "Display the images in DS9?",action='store_true')
+	args=parser.parse_args()
+	return args
+
+args=argParse()
+outdir="/Users/James/Documents/PythonScripts/NGTS/Repo/MoonAvoidance"
 
 def estimate_exp_coeffs(counts):
 	return [max(counts)*2,0.154,min(counts)]
@@ -13,30 +28,97 @@ def estimate_exp_coeffs(counts):
 def exp_func(x, a, c, d):
     return a*np.exp(-c*x)+d
 
-t=g.glob('*.fits')
-moon_ang=np.empty(len(t))
-moon_phase=np.empty(len(t))
-median_counts=np.empty(len(t))
-for i in range(0,len(t)):
-	h=fits.open(t[i])
-	d=h[0].data
-	median_counts[i]=np.median(d)
-	moon_ang[i]=h[0].header['MOONDIST']
-	moon_phase[i]=h[0].header['MOONFRAC']
+def getMoonData():
+	t=g.glob('*.fits')
+	moon_ang=np.empty(len(t))
+	moon_phase=np.empty(len(t))
+	median_counts=np.empty(len(t))
+	for i in range(0,len(t)):
+		h=fits.open(t[i])
+		d=h[0].data
+		prescan=d[0:2048,:20]
+		overscan=d[0:2048,2068:]
+		data=d[0:2048,20:2068]
+		median_counts[i]=np.median(data)-np.median(overscan)
+		moon_ang[i]=h[0].header['MOONDIST']
+		moon_phase[i]=h[0].header['MOONFRAC']
+		h.close()
+	return t,median_counts,moon_ang,moon_phase
 
-# fit exponential decay to the data - not quite right but close enough
-p0=estimate_exp_coeffs(median_counts)
-popt, pcov = curve_fit(exp_func, moon_ang, median_counts, p0)
-xfit=np.linspace(0,180,180)
-yfit=exp_func(xfit,*popt)
+def fitMoonData(p0,moon_ang,median_counts):
+	popt, pcov = curve_fit(exp_func, moon_ang, median_counts, p0)
+	xfit=np.linspace(0,180,180)
+	yfit=exp_func(xfit,*popt)
+	return popt,pcov,xfit,yfit
 
-fig,ax=pl.subplots(1,figsize=(10,10))
-seaborn.axes_style("darkgrid")
-ax.plot(moon_ang,median_counts,'r.')
-ax.plot(xfit,yfit,'k--')
-ax.set_xlabel('Moon Distance (deg)')
-ax.set_ylabel('Median ADU (image)')
-ax.set_title('Moon Avoidance Test (%s - %d%% illuminated)' % (os.getcwd().split('/')[-1], int(np.average(moon_phase)*100)))
-ax.set_xlim(0,180)
-ax.grid(True)
-pl.show()
+def plotMoonDataFit(moon_ang,median_counts,xfit,yfit,moon_phase):
+	action=os.getcwd().split('/')[-1]
+	fig,ax=pl.subplots(1,figsize=(10,10))
+	seaborn.axes_style("darkgrid")
+	ax.plot(moon_ang,median_counts,'r.')
+	ax.plot(xfit,yfit,'k--')
+	ax.set_xlabel('Moon Distance (deg)')
+	ax.set_ylabel('Median ADU (image)')
+	ax.set_yticks(np.arange(0,max(yfit),2000))
+	ax.set_title('Moon Avoidance Test (%s - %d%% illuminated)' % (action, int(np.average(moon_phase)*100)))
+	ax.set_xlim(0,180)
+	ax.set_ylim(0,max(yfit))
+	ax.grid(True)
+	fig.savefig("%s/MoonAvoidance_%s.png" % (outdir,action), dpi=300)
+	return action
+
+# check for ghosts on images where MOONDIST <= ghostlim
+def checkGhostLimit(moon_ang,t,action):
+	check_img,check_ang=[],[]
+	for i in range(0,len(moon_ang)):
+		if moon_ang[i] <= args.ghostlim:
+			check_ang.append(moon_ang[i])
+			check_img.append(t[i])
+	temp=zip(check_ang,check_img)
+	temp.sort()
+	check_ang_s,check_img_s=zip(*temp)
+
+	if args.ds9:
+		ds=ds9()
+		time.sleep(5)
+		ds.set('scale zscale')
+		ds.set('preserve scale')
+		ds.set('preserve pan')
+		artefacts=[]
+		for i in range(0,len(check_img_s)):
+			ds.set('tile yes')
+			ds.set('frame 1')
+			ds.set('file %s' % (check_img_s[i]))
+			ds.set('zoom to fit')
+			ds.set('cmap invert yes')
+			print "%s MOONDIST: %.2f" % (check_img_s[i],check_ang_s[i])
+			next_yn=raw_input("Are the image artefacts? e.g. (y/n): ")
+			artefacts.append(next_yn.lower())
+
+	fig = pl.figure(2,figsize=(15,15))
+	img_stack=[]
+	montage_dim=int(ceil(sqrt(len(check_img_s))))
+	c=0
+	for i in range(0,montage_dim):
+		for j in range(0,montage_dim):
+			ax = fig.add_subplot(montage_dim, montage_dim, c+1, xticks=[], yticks=[])
+			if c < len(check_img_s):
+				cd=fits.open(check_img_s[c])[0].data
+				cdata=cd[0:2048,20:2068]
+				coscan=cd[0:2048,2068:]
+				cdcor=cdata-np.median(coscan)
+				ax.imshow(cdcor,cmap=cm.afmhot,vmin=0.8*np.median(cdcor),vmax=1.2*np.median(cdcor),interpolation=None)        
+				ax.set_title('MOONDIST: %.2f' % (check_ang_s[c]))
+			c+=1
+	fig.savefig('%s/GhostCheck-%d_%s.png' % (outdir,args.ghostlim,action),dpi=300)
+
+def main():
+	t,median_counts,moon_ang,moon_phase=getMoonData()
+	p0=estimate_exp_coeffs(median_counts)
+	popt,pcov,xfit,yfit=fitMoonData(p0,moon_ang,median_counts)
+	action=plotMoonDataFit(moon_ang,median_counts,xfit,yfit,moon_phase)
+	if args.ghostlim:
+		checkGhostLimit(moon_ang,t,action)
+
+if __name__ == '__main__':
+	main()
