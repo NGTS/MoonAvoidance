65028a48877a221c464e46a961a9844c11962cca
jmccormac01 <jmccormac001@gmail.com>
2016-02-12 10:08:11 +0000

    bug fix in loop

--- findActionsFromImgIds.py
+++ findActionsFromImgIds.py
@@ -4,7 +4,7 @@ db=pymysql.connect(host='ngtsdb',db='ngts_ops')
 img=np.loadtxt('pre_baffle_imglist.txt',usecols=[0],unpack=True)
 action_id=[]
 for i in img:
-	qry="SELECT action_id FROM raw_image_list WHERE image_id=%d LIMIT 1" % img
+	qry="SELECT action_id FROM raw_image_list WHERE image_id=%d LIMIT 1" % i
 	with db.cursor() as cur:
 		cr.execute(qry)	
 		for row in cur:
