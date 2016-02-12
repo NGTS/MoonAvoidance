8b3da4441fa76bb7a6817764bac6b19a8b27acfd
jmccormac01 <jmccormac001@gmail.com>
2016-02-12 10:17:28 +0000

    need bigger ints

--- findActionsFromImgIds.py
+++ findActionsFromImgIds.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pymysql
 db=pymysql.connect(host='ngtsdb',db='ngts_ops')
-img=np.loadtxt('pre_baffle_imglist.txt',usecols=[0],unpack=True).astype(int)
+img=np.loadtxt('pre_baffle_imglist.txt',usecols=[0],unpack=True).astype(np.int64)
 action_id=[]
 for i in img:
 	qry="SELECT action_id FROM raw_image_list WHERE image_id=%d LIMIT 1" % i
