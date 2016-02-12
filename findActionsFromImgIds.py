3fd512b1f6834e85ce44f03f5e8f9ee73d891b76
jmccormac01 <jmccormac001@gmail.com>
2016-02-12 10:19:35 +0000

    using strings instead of numbers, digits are being changed

--- findActionsFromImgIds.py
+++ findActionsFromImgIds.py
@@ -1,10 +1,13 @@
 import numpy as np
 import pymysql
 db=pymysql.connect(host='ngtsdb',db='ngts_ops')
-img=np.loadtxt('pre_baffle_imglist.txt',usecols=[0],unpack=True).astype(np.int64)
-action_id=[]
+f=open('pre_baffle_imglist.txt','r').readlines()
+img,action_id=[],[]
+for i in f:
+	img.append(i.split('\n')[0])
+f.close()
 for i in img:
-	qry="SELECT action_id FROM raw_image_list WHERE image_id=%d LIMIT 1" % i
+	qry="SELECT action_id FROM raw_image_list WHERE image_id=%s LIMIT 1" % i
 	with db.cursor() as cur:
 		cur.execute(qry)	
 		for row in cur:
